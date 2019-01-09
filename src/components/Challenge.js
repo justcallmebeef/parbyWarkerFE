c1c8f3642addd80e965467c919474346608dd6ee
justcallmebeef <bethanymitch10@gmail.com>
2019-01-09 13:54:49 -0700

    changed unordered list to be a table and added css to clean it up. added an update and delete button and an appropriate router to said update

--- src/components/Challenge.js
+++ src/components/Challenge.js
@@ -4,7 +4,11 @@ import ProductTable from './ProductTable'
 const Challenge = (props) => {
     return (
         <Fragment>
+            <div className="challengeContainer">
+            <h1>The Challenge</h1>
+            <p>Click below to see more details.</p>
             <ProductTable getProfile={props.getProfile} getMonth={props.getMonth} products={props.products}/>
+            </div>
         </Fragment>
     )
 }
--- src/components/ProductTable.js
+++ src/components/ProductTable.js
@@ -6,26 +6,33 @@ const ProductTable = (props) => {
         var date = new Date(product.last_modified).getDate()
         var month = props.getMonth(new Date(product.last_modified).getMonth())
         var year = new Date(product.last_modified).getFullYear()
-        
+
         return (
             <Fragment>
-                <div>
-                    <div>
-                    <Link to={`/productprofile/${product.name}`}><ul onClick={() => props.getProfile(product.id) }>
-                            <li>Parby Warker {product.name}</li>
-                            <li>${product.price.toFixed(2)}</li>
-                            <li>{product.code}</li>
-                            <li>{product.creator}</li>
-                            <li>{month} {date}, {year}</li>
-                        </ul></Link>
-                    </div>
-                </div>
+                    <tr onClick={() => props.getProfile(product.id) } className="productTable">
+                        <td id="productListing">Parby Warker {product.name}</td>
+                        <td id="priceListing">${product.price.toFixed(2)}</td>
+                        <td id="codeListing">{product.code}</td>
+                        <td id="creatorListing">{product.creator}</td>
+                        <td id="dateListing">{month} {date}, {year}</td>
+                        <td><Link to={`/productprofile/${product.name}`} style={{ textDecoration: 'none', color: '#333333' }}><button>Update</button></Link></td>
+                        <td><button>Delete</button></td>
+                    </tr>
             </Fragment>
         )
     })
         return (
             <Fragment>
+                <table>
+                    <tr>
+                        <th>PRODUCT</th>
+                        <th>PRICE</th>
+                        <th>CODE</th>
+                        <th>CREATED BY</th>
+                        <th>LAST MODIFIED</th>
+                    </tr>
                 { productList }
+                </table>
             </Fragment>
         )
 }
